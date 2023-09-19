36a86656af4c5bb97e3cec4c8cb14ff5157e060e
Bonaiti.20104 <Bonaiti.20104@LABI2-13PC13>
2023-09-19 11:54:06 +0200

    esercizio di ripasso svolto in c#

--- ESERCIZIO 12.09/Program.cs
+++ ESERCIZIO 12.09/Program.cs
@@ -10,23 +10,24 @@ namespace ESERCIZIO_12._09
     {
         static void Main(string[] args)
         {
-            int[] arr1 = new int[100];    //dichiarazione dell'array array1
-            int c, n, p;                    //dichiarazione delle variabili c, n, p; rispettivamente la variabile della posizione dell'ultimo valore, la variabile che degli gli input e la variabile della posizione ricevuta in input
-            bool r = false;                 //variabile booleana per l'uscita dal ciclo do while
-            do                              //ciclo do while che verifica che l'input ricevuto sia compreso tra 0 e 25(per questione di comodità)
+            int c, n, p, m, g, ng;                    //dichiarazione delle variabili c, n, p; rispettivamente la variabile della posizione dell'ultimo valore, la variabile che degli gli input e la variabile della posizione ricevuta in input
+            Random rnd = new Random();
+            Console.WriteLine("Quanti numeri vuoi generare?");
+            int[] arr1 = new int[Convert.ToInt16(Console.ReadLine())];
+            Console.WriteLine("Inserire il valore più piccolo dei numeri da generare");
+            m = int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire il valore più grande dei numeri da generare");
+            g = int.Parse(Console.ReadLine());
+            c = arr1.Length;
+            for (int i = 0; i < arr1.Length; i++)
             {
-                Console.WriteLine("Inserire numero di elementi iniziale: (0 < N < 25)");    //output del messaggio "Inserire numero di elementi iniziale: (0 < N < 25)"
-                c = int.Parse(Console.ReadLine());                                          //assegnazione a c del valore ricevuto in input dall'utente
-            } while (c < 0 || c > 25);                                                      //codizione che verifica che c sia <0 o >25 per ripetere il ciclo
-            for (byte i = 0; i < c; i++)                                                    //ciclo for per chiedere all'utente l'input c volte
-            {
-                Console.WriteLine($"Inserire elemento iniziale in posizione {i}: ");        //output del messaggio "Inserire elemento iniziale in posizione {i}: " con {i} sostituito dal valore di i
-                arr1[i] = int.Parse(Console.ReadLine());                                  //salvataggio in array1[i] del valore ricevuto in input
-            }
+                ng = rnd.Next(m, g);
+            }                        //dichiarazione dell'array array
+            bool r = false;       
             Console.Clear();    //reset della console
             do                  //ciclo do while che verifica se ripetere il programma o chiuderlo
             {
-                Console.WriteLine("Inserire la lettera associata alla funzione per selezionarla: \r\na - Aggiungere un elemento in coda all'array\r\nv - Visualizzare l'array che restituisca la stringa in HTML\r\nr - Ricerca un numero all'interno dell'array\r\nc - Cancellazione di un elemento nell'array\r\ni - Inserimento di un valore in una posizione dell'array\r\ns - Stampa dell'array aggiornato\r\nu - Uscita dal programma"); //output delle indicazioni
+               Console.WriteLine("Inserire la lettera associata alla funzione per selezionarla: \r\na - Aggiungere un elemento in coda all'array\r\nv - Visualizzare l'array che restituisca la stringa in HTML\r\nr - Ricerca un numero all'interno dell'array\r\nc - Cancellazione di un elemento nell'array\r\ni - Inserimento di un valore in una posizione dell'array\r\ns - Stampa dell'array aggiornato\r\nt - Troncamento dell'array nella posizione desiderata\r\nz - Inserimento di numeri ordinati nell'array\r\nu - Uscita dal programma"); //output delle indicazioni
                 switch (Console.ReadLine())         //switch che verifica l'input ricevuto
                 {
                     case "a":
@@ -83,6 +84,29 @@ namespace ESERCIZIO_12._09
                         }
                         Console.WriteLine();
                         break;
+                    case "t":
+                        for (int i = 0; i < c; i++)
+                        {
+                            Console.Write(arr1[i] + " ");
+                        }
+                        Console.WriteLine();
+                        break;
+                    case "z":
+                        int[] arr = new int[100];
+                        int[] arr2 = new int[arr.Length+arr1.Length];
+                        do                              //ciclo do while che verifica che l'input ricevuto sia compreso tra 0 e 25(per questione di comodità)
+                        {
+                            Console.WriteLine("Inserire numero di elementi iniziale: (0 < N < 25)");    //output del messaggio "Inserire numero di elementi iniziale: (0 < N < 25)"
+                            c = int.Parse(Console.ReadLine());                                          //assegnazione a c del valore ricevuto in input dall'utente
+                        } while (c < 0 || c > 25);                                                      //codizione che verifica che c sia <0 o >25 per ripetere il ciclo
+                        for (byte i = 0; i < c; i++)                                                    //ciclo for per chiedere all'utente l'input c volte
+                        {
+                            Console.WriteLine($"Inserire elemento iniziale in posizione {i}: ");        //output del messaggio "Inserire elemento iniziale in posizione {i}: " con {i} sostituito dal valore di i
+                            arr[i] = int.Parse(Console.ReadLine());                                  //salvataggio in array1[i] del valore ricevuto in input
+                        }
+                        Console.WriteLine(Insor(arr1, arr, ref arr2));
+                        Console.WriteLine();
+                        break;                       
                     case "u":
                         r = true;
                         break;
@@ -179,5 +203,58 @@ namespace ESERCIZIO_12._09
             }
             return inserito;
         }
+        static int[] Insor(int[] array1,int[] array2,ref int[] array3)
+        {
+            int i = 0, j = 0, k = 0;
+            for (; i < array1.Length && j < array2.Length; k++)
+            {
+                if (array1[i] < array2[j])
+                {
+                    array3[k] = array1[i];
+                    i++;
+                }
+                else
+                {
+                    array3[k] = array2[j];
+                    j++;
+                }
+            }
+            if (i == array1.Length)
+            {
+                for (; j < array2.Length; j++)
+                {
+                    array3[k] = array2[j];
+                    k++;
+                }
+            }
+            else
+            {
+                for (; i < array1.Length; i++)
+                {
+                    array3[k] = array1[i];
+                    k++;
+                }
+            }
+            return array3;
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
